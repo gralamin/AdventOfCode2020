5c987196b7256a1903d7e74e584217fa748f50c3
Glen Nelson <gralamin@gralamin.com>
2020-12-06 14:03:26 -0700

    day6: Answer, simple intersection

--- day6/answer.py
+++ day6/answer.py
@@ -1,12 +1,22 @@
 class Customs:
     def __init__(self):
         self.set = set()
+        self.part2Set = set()
+        self.first = True
 
     def parse_string(self, str):
         self.set = self.set.union(set(str.strip()))
+        if self.first:
+            self.part2Set = self.part2Set.union(set(str.strip()))
+            self.first = False
+        else:
+            self.part2Set = self.part2Set.intersection(set(str.strip()))
 
     def count(self):
         return len(self.set)
+
+    def count2(self):
+        return len(self.part2Set)
 
 
 def parse_input():
@@ -21,4 +31,6 @@ def parse_input():
         yield c
 
 
-print(sum(x.count() for x in parse_input()))
+inputs = [x for x in parse_input()]
+print("Part1: ", sum(x.count() for x in inputs))
+print("Part2: ", sum(x.count2() for x in inputs))
