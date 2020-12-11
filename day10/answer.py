bf9237c1117f7ea22fe7864b8456afddf6405b93
Glen Nelson <gralamin@gralamin.com>
2020-12-10 20:34:27 -0700

    Day10: answer part2

--- day10/answer.py
+++ day10/answer.py
@@ -8,7 +8,8 @@ with open("input", "r") as f:
 differences = {}
 
 sorted_jolts = sorted(jolts)
-sorted_jolts.append(sorted_jolts[-1] + max_of_all_voltage_modifer)
+device = sorted_jolts[-1] + max_of_all_voltage_modifer
+sorted_jolts.append(device)
 
 cur_joltage = 0
 for j in sorted_jolts:
@@ -19,3 +20,30 @@ for j in sorted_jolts:
     cur_joltage = j
 
 print("Part 1: ", differences[1] * differences[3])
+
+# Part 2 is a bit more complex...
+# Easiest solution I can think of is building a graph.
+
+
+def graph(inputs):
+    neighbors = {}
+    # O(n^2), because for each value, we look in the list for another value.
+    for j in inputs:
+        potential_neighbors = [j + x for x in [1, 2, 3]]
+        neighbors[j] = [n for n in potential_neighbors if n in inputs]
+    return neighbors
+
+
+# Add 0 base state for graph
+sorted_jolts = [0] + sorted_jolts
+paths = graph(sorted_jolts)
+
+# Now, count the paths
+# We have 1 way to get to 0, as its our start.
+counted_paths = {0: 1}
+for j, neighbors in paths.items():
+    for n in neighbors:
+        counted_paths[n] = counted_paths.setdefault(n, 0) + counted_paths[j]
+
+# Then check how many are at the device
+print("Part 2", counted_paths[device])
