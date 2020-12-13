2a7e4fccace0d397c9d24dc7070b3f246623a30d
Glen Nelson <gralamin@gralamin.com>
2020-12-13 11:40:43 -0700

    day13: part 2 answer

--- day13/test_answer.py
+++ day13/test_answer.py
@@ -9,3 +9,13 @@ class TestIntegration(TestCase):
 
     def test_part_one(self):
         self.assertEqual(answer.part_1(self.start_timestamp, self.buses), 295)
+
+    def test_part_two(self):
+        self.assertEqual(answer.part_2(self.buses), 1068781)
+
+    def test_part_two_other_examples(self):
+        self.assertEqual(answer.part_2("17,x,13,19".split(",")), 3417)
+        self.assertEqual(answer.part_2("67,7,59,61".split(",")), 754018)
+        self.assertEqual(answer.part_2("67,x,7,59,61".split(",")), 779210)
+        self.assertEqual(answer.part_2("67,7,x,59,61".split(",")), 1261476)
+        self.assertEqual(answer.part_2("1789,37,47,1889".split(",")), 1202161486)
