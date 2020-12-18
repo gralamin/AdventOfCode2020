1a9ac699483cf2f12b64ecf07882f4eb1e4dcce8
Glen Nelson <gralamin@gralamin.com>
2020-12-17 18:43:54 -0700

    Day17: Part 2 answer, but slow

--- day17/test_answer.py
+++ day17/test_answer.py
@@ -4,24 +4,23 @@ import answer
 
 class TestIntegration(TestCase):
     def test_part_1(self):
-        input_state = answer.CubeGrid3D(
-            [
-                answer.ConwayCube(
-                    answer.Coordinate3D(1, 0, 0), answer.CubeState.ACTIVE
-                ),
-                answer.ConwayCube(
-                    answer.Coordinate3D(2, 1, 0), answer.CubeState.ACTIVE
-                ),
-                answer.ConwayCube(
-                    answer.Coordinate3D(0, 2, 0), answer.CubeState.ACTIVE
-                ),
-                answer.ConwayCube(
-                    answer.Coordinate3D(1, 2, 0), answer.CubeState.ACTIVE
-                ),
-                answer.ConwayCube(
-                    answer.Coordinate3D(2, 2, 0), answer.CubeState.ACTIVE
-                ),
-            ]
-        )
-        answer.part_1(input_state, num_cycles=3, debug=True)
+        input_state = [
+            answer.ConwayCube(answer.Coordinate3D(1, 0, 0), answer.CubeState.ACTIVE),
+            answer.ConwayCube(answer.Coordinate3D(2, 1, 0), answer.CubeState.ACTIVE),
+            answer.ConwayCube(answer.Coordinate3D(0, 2, 0), answer.CubeState.ACTIVE),
+            answer.ConwayCube(answer.Coordinate3D(1, 2, 0), answer.CubeState.ACTIVE),
+            answer.ConwayCube(answer.Coordinate3D(2, 2, 0), answer.CubeState.ACTIVE),
+        ]
+        # answer.part_1(input_state, num_cycles=3, debug=True)
         self.assertEqual(answer.part_1(input_state, num_cycles=6, debug=False), 112)
+
+    def test_part_2(self):
+        input_state = [
+            answer.ConwayCube(answer.Coordinate3D(1, 0, 0), answer.CubeState.ACTIVE),
+            answer.ConwayCube(answer.Coordinate3D(2, 1, 0), answer.CubeState.ACTIVE),
+            answer.ConwayCube(answer.Coordinate3D(0, 2, 0), answer.CubeState.ACTIVE),
+            answer.ConwayCube(answer.Coordinate3D(1, 2, 0), answer.CubeState.ACTIVE),
+            answer.ConwayCube(answer.Coordinate3D(2, 2, 0), answer.CubeState.ACTIVE),
+        ]
+        # answer.part_2(input_state, num_cycles=3, debug=True)
+        self.assertEqual(answer.part_2(input_state, num_cycles=6, debug=False), 848)
