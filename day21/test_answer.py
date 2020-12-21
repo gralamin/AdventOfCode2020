c4e50dbfbf78adbbdb8dae82b98b0bd5ba707afa
Glen Nelson <gralamin@gralamin.com>
2020-12-21 16:52:18 -0700

    day21: add answer

---
+++ day21/test_answer.py
@@ -0,0 +1,15 @@
+from unittest import TestCase
+import answer
+
+EXAMPLE_INPUTS = """mxmxvkd kfcds sqjhc nhms (contains dairy, fish)
+trh fvjkl sbzzf mxmxvkd (contains dairy)
+sqjhc fvjkl (contains soy)
+sqjhc mxmxvkd sbzzf (contains fish)"""
+
+
+class TestIntegration(TestCase):
+    def setUp(self):
+        self.foods = [answer.Food(x) for x in EXAMPLE_INPUTS.split("\n")]
+
+    def test_part_1(self):
+        self.assertEqual(answer.part1(self.foods), 5)
